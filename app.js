cedaf4d0a217d80b50b92ba841ae2f2a86d17f77
rahulmhjn <rahul.mahajan676@gmail.com>
2019-02-08 20:13:59 +0530

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,11 +30,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req,res,next) {
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user) {
+  console.log(req.session);
+  if(!req.session.user) {
     var authHeader = req.headers.authorization;
     if(!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -48,7 +57,8 @@ function auth(req,res,next) {
     var password = auth[1];
 
     if(username == 'admin' && password == 'password') {
-      res.cookie('user', 'admin', { signed:true });
+      //res.cookie('user', 'admin', { signed:true });
+      req.session.user = 'admin';
       next();
     }
     else {
@@ -60,7 +70,7 @@ function auth(req,res,next) {
     }
   }
   else {
-    if(req.signedCookies.user == 'admin') {
+    if(req.session.user == 'admin') {
       next();
     }
     else {
